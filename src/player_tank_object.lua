abdb1e9a3b97b24f89b762ba837572c13dd1194b
beardog-ukr <you@example.com>
2019-03-22 10:31:33 +0200

    Added: player tank moves

--- src/player_tank_object.lua
+++ src/player_tank_object.lua
@@ -13,9 +13,9 @@ tankDirections.down =2
 tankDirections.left =3
 tankDirections.right =4
 
-local tankDirectionAngles = {math.pi, 
-                             0, 
-                             math.pi/2, 
+local tankDirectionAngles = {math.pi,
+                             0,
+                             math.pi/2,
                              -math.pi/2} -- for up, down, left,right
 
 local keyBindings = {}
@@ -29,17 +29,17 @@ keyBindings.fire = "space"
 
 function PlayerTankObject:constructor(x,y, symbol)
   BasicGameObject.constructor(self, x,y)
-  
+
   self.direction = tankDirections.down
   self.moveProgress = 0
-
+  self.nextMove = nil
 end
 
 function PlayerTankObject:getAngle()
   return tankDirectionAngles[self.direction]
 end
 
-function PlayerTankObject:getDrawCoordinates()  
+function PlayerTankObject:getDrawCoordinates()
   local resultX = self.baseX
   local resultY = self.baseY
 
@@ -57,39 +57,15 @@ function PlayerTankObject:getDrawCoordinates()
 end
 
 function PlayerTankObject:getImageId()
-  return 10  
+  return 10
 end
 
 -- ===========================================================================
 
--- local function performMove(tankCtx, maxLim, walls)
---   if (tankCtx.cellY == 13) then
---     LL.debug("Enemy already at max south")
---     return
---   end
-
---   if (walls[tankCtx.cellY+1][tankCtx.cellX] ~= mapLegend.space) then
---     LL.debug("Wall at south from enemy, no way")
---     return
---   end
-
---   local function postEnemyDownMove()
---     LL.debug("Enemy one move to south finished")
---     tankCtx.moveProgress=0    
---     tankCtx.cellY = tankCtx.cellY +1
---   end
-
---   tankCtx.direction = tankDirections.down
---   TimerKnife.tween(delays.move, 
---                    { [tankCtx] = { moveProgress = 1 } }):finish(postEnemyDownMove) 
--- end
-
--- local moveKeys = {keyBindings.up, keyBindings.down, keyBindings.left, keyBindings.right }
--- local moveMaxLim = {1, 13, 1, 13 }
-
 function PlayerTankObject:processMoveRequest(key, walls)
   if (self.moveProgress > 0.05) then
     LL.warn("Still moving: " .. self.moveProgress)
+    self.nextMove = key
     return
   end
 
@@ -107,7 +83,7 @@ function PlayerTankObject:processMoveRequest(key, walls)
     newBaseX = newBaseX -1
     newDirection = tankDirections.left
   elseif (key == keyBindings.right) then
-    newBaseX = newBaseY +1
+    newBaseX = newBaseX +1
     newDirection = tankDirections.right
   end
 
@@ -126,13 +102,18 @@ function PlayerTankObject:processMoveRequest(key, walls)
 
   local function postTankMove()
     LL.debug("one move to " .. key .. " finished")
-    self.moveProgress=0    
+    self.moveProgress=0
     self.baseX = newBaseX
     self.baseY = newBaseY
+    if (self.nextMove) then
+      LL.debug("initiate next move to " .. self.nextMove)
+      self:processMoveRequest(self.nextMove, walls)
+      self.nextMove = nil
+    end
   end
 
   self.direction = newDirection
-  TimerKnife.tween(5, 
+  TimerKnife.tween(2,
                    { [self] = { moveProgress = 1 } }):finish(postTankMove)
 
 end
